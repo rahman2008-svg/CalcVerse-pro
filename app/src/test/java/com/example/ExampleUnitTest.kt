865f5b7630d0a6f6acd5b523c3104fe5ba78ace1
Abdur Rahman <prince.ar.abdur.rahman2008@gmail.com>
2026-05-31 09:55:57 +0600

    Update ExampleUnitTest.kt

--- app/src/test/java/com/example/ExampleUnitTest.kt
+++ app/src/test/java/com/example/ExampleUnitTest.kt
@@ -1,14 +1,13 @@
-package com.example
+package com.calcverse.pro   // ← Fixed
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
+ * Example local unit test for CalcVerse Pro.
  */
 class ExampleUnitTest {
+
   @Test
   fun addition_isCorrect() {
     assertEquals(4, 2 + 2)
